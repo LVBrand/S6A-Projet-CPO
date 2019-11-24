e31d8a692c9bf54361a6d2340f1e7d710d46c2d2
Lucas Brand <l6brand@enib.fr>
2019-11-25 00:25:26 +0100

    Add LevelManager script v.1
    J'ai ajouté un nouveau script LevelManager sensé remplacé le petit script GenerationMap qui faisait globalement la même chose mais avec des fonctions que je ne maitrisais pas vraiment. Du coup j'ai commencé à faire LevelManager et j'ai réussi à générer la map (plage + mer).

---
+++ Counter-Wastes/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LevelManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] tilePrefabs;
+
+    // calculer la taille des tiles, on l'utilise pour bien positionner les tiles 
+    public float TileSize
+    {
+        get { return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateLevel();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void CreateLevel()
+    {
+        // instanciation temporaire de la tilemap, on utilise un documen texte pour le remplacer
+        string[] mapData = ReadLevelText();
+
+        // Calcul de x (map)
+        int mapSizeX = mapData[0].ToCharArray().Length;
+
+        // Calcul de y (map)
+        int mapSizeY = mapData.Length;
+
+        //On calcule le point de départ du la map, le point en haut à gauche en gros
+        Vector2 worldStart = Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height));
+
+        for (int y = 0; y < mapSizeY; y++) // positions en y (8)
+        {
+            //on prend tous les char de mapdata et on en fait un tableau
+            char[] newTiles = mapData[y].ToCharArray();
+
+            for (int x = 0; x < mapSizeX; x ++) // positions en x (15)
+            {
+                PlaceTile(newTiles[x].ToString(),x,y,worldStart);
+            }
+        }
+    }
+
+
+    private void PlaceTile(string tileType, int x, int y, Vector2 worldStart)
+    {
+        // par exemple : "1" devient 1
+        int tileIndex = int.Parse(tileType);
+
+        // Création d'un nouveau tile et référencement de ce dernier dans la variable newTile
+        GameObject newTile = Instantiate(tilePrefabs[tileIndex]);
+
+        // Utilisation de la variable newTile pour changer la position du tile
+        newTile.transform.position = new Vector2(worldStart.x + (TileSize * x), worldStart.y - (TileSize * y));
+    }
+
+    
+    private string[] ReadLevelText()
+    {
+        TextAsset bindData = Resources.Load("Level") as TextAsset;
+
+        string data = bindData.text.Replace(Environment.NewLine, string.Empty);
+
+        //on coupe par le caractère "-" dans notre fichier texte
+        return data.Split('-');
+
+    }
+}
