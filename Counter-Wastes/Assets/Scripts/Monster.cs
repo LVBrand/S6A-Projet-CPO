ea54069e407a530208977684acfa0b8a3a07e7f9
OscarAFK <57917670+OscarAFK@users.noreply.github.com>
2019-12-01 14:33:07 +0100

    Déplacement des monstres
    Ils se déplacent, c'est cool et tout.

--- Counter-Wastes/Assets/Scripts/Monster.cs
+++ Counter-Wastes/Assets/Scripts/Monster.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
+
+    [SerializeField]
+    private float speed;
+
+    public Point GridPosition { get; set; }
+
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 destination = new Vector2(-5, transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position,destination, speed * Time.deltaTime);
+
+        if((Vector2)transform.position == destination)
+        {
+
+        }
+    }
+
     public void Spawn()
     {
         int nbLane = Random.Range(0, 6);
