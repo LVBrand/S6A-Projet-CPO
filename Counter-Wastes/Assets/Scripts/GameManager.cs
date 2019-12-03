a5db239a09b5e8dbb7b247e7a0c8e15394dbaf18
Lucas Brand <l6brand@enib.fr>
2019-12-03 22:10:48 +0100

    Added Currency

--- Counter-Wastes/Assets/Scripts/GameManager.cs
+++ Counter-Wastes/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : Singleton<GameManager>
 {
@@ -10,11 +11,33 @@ public class GameManager : Singleton<GameManager>
         get; set;
     }
 
+    private int currency;
+
+    [SerializeField]
+    private Text currencyTxt;
+
+
+
+    public int Currency
+    {
+        get
+        {
+            return currency;
+        }
+
+        set
+        {
+            this.currency = value;
+            this.currencyTxt.text = "<color=lime>$</color>" + value.ToString();
+        }
+    }
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Currency = 50;
     }
 
     // Update is called once per frame
@@ -26,13 +49,24 @@ public class GameManager : Singleton<GameManager>
 
     public void PickTower(TowerButton towerButton)
     {
-        this.ClickedButton = towerButton;
-        Hover.Instance.Activate(towerButton.Sprite);
+        if (Currency >= towerButton.Price)
+        {
+            //Stores the clicked button
+            this.ClickedButton = towerButton;
+            //active l'icone de Hover
+            Hover.Instance.Activate(towerButton.Sprite);
+        }
+
     }
 
     public void BuyTower()
     {
-        Hover.Instance.Deactivate();
+        if (Currency >= ClickedButton.Price)
+        {
+            Currency -= ClickedButton.Price;
+            Hover.Instance.Deactivate();
+        }
+
     }
 
 
