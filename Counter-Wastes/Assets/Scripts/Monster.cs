16715188aa6397b75cd90bfea6e61bd0aec4252d
OscarAFK <57917670+OscarAFK@users.noreply.github.com>
2019-12-13 11:59:21 +0100

    Ajout animation
    C'est dégeulasse, faudra changer les courbes mais autrement ça marche

--- Counter-Wastes/Assets/Scripts/Monster.cs
+++ Counter-Wastes/Assets/Scripts/Monster.cs
@@ -24,6 +24,12 @@ public class Monster : MonoBehaviour
 
     private float speed;
 
+    public AnimationCurve yCurve, lCurve, hCurve;
+    private float timeElapsed = 0;
+    private float YstartPosition;
+    private float LstartScale;
+    private float HstartScale;
+
     public float Life
     {
         get
@@ -47,15 +53,28 @@ public class Monster : MonoBehaviour
     private void Update()
     {
         Move();
+        animationUpdate();
     }
 
+    private void animationUpdate()
+    {
+        Debug.Log(timeElapsed);
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed >= yCurve[yCurve.length - 1].time) timeElapsed = 0;
+        transform.position = new Vector2(transform.position.x, YstartPosition + 0.3f*yCurve.Evaluate(timeElapsed));
+        transform.localScale = new Vector2(LstartScale + 1f * lCurve.Evaluate(timeElapsed), HstartScale + 1f * hCurve.Evaluate(timeElapsed)); ;
+    }
     public void Spawn()
     {
+        timeElapsed = 0;
         int nbLane = Random.Range(0, 6);
         transform.position = LevelManager.Instance.spawn[nbLane].transform.position;
         life = maxLife;
         this.speed = maxSpeed;
         transform.GetComponent<Renderer>().material.color = new Color(1, 1, 1);
+        YstartPosition = transform.position.y;
+        LstartScale = transform.localScale.x;
+        HstartScale = transform.localScale.y;
     }
 
     private void Move()
