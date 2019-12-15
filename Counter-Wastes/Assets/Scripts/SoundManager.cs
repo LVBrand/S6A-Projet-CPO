f16b96c89d782a9897a1b1634c5658db024009f3
Lucas Brand <l6brand@enib.fr>
2019-12-15 15:57:48 +0100

    SoundManager

---
+++ Counter-Wastes/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource musicSource;
+
+    [SerializeField]
+    private AudioSource sfxSource;
+
+    [SerializeField]
+    private Slider sfxSlider;
+
+    [SerializeField]
+    private Slider MusicSlider;
+
+    Dictionary<string, AudioClip> audioClips = new Dictionary<string, AudioClip>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioClip[] clips = Resources.LoadAll<AudioClip>("sounds") as AudioClip[];
+
+        foreach (AudioClip clip in clips)
+        {
+            audioClips.Add(clip.name, clip);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            PlaySFX("ammo_pickup");
+        }
+    }
+
+    public void PlaySFX(string name)
+    {
+        sfxSource.PlayOneShot(audioClips[name]);
+    }
+}
